33f85ed9ff72bfa9552b2ebe39b8c9985446f8cc
Pritam Sarbajna <pritamsarbajna10@gmail.com>
2022-11-05 23:34:21 +0530

    Added SECRET_ENV

--- hotel/settings.py
+++ hotel/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-40!g4*4m^c5&6h4b1ymk4&a(-4eejh%3in(-=1)&zp94c$71@g'
+SECRET_KEY = os.getenv('SECRET_ENV')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
